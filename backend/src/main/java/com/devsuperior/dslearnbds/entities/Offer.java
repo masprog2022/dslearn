cc2dd7744f1486bd33dba89e5283858d4deef7df
masprog2022 <masprog2022@gmail.com>
2023-01-10 21:16:40 +0100

    :sparkles: feat: add entity Resource

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
@@ -2,6 +2,8 @@ package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -34,6 +37,9 @@ public class Offer implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "course_id")
 	private Course course;
+	
+	@OneToMany(mappedBy = "offer")
+	private List<Resource> resources = new ArrayList<>();
 	
 	
 	public Offer() {
@@ -87,6 +93,12 @@ public class Offer implements Serializable{
 
 	public void setCourse(Course course) {
 		this.course = course;
+	}
+	
+	
+
+	public List<Resource> getResources() {
+		return resources;
 	}
 
 	@Override
