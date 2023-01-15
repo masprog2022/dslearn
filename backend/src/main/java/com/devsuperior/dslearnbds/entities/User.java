3b1e1c798c055260143b2ddefe71886c90796d31
masprog2022 <masprog2022@gmail.com>
2023-01-15 01:28:37 +0100

    :boom: fix: remove user Role on construtor

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
@@ -51,7 +51,7 @@ public class User implements Serializable {
 		
 	}
 
-	public User(Long id, String name, String email, String password, Set<Role> roles) {
+	public User(Long id, String name, String email, String password) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
