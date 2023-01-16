036e30ac004d38ac1b28871be6792d4228dbb6ca
masprog2022 <masprog2022@gmail.com>
2023-01-16 15:10:03 +0100

    :sparkles: feat: add Custom service level authorization

--- backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
@@ -25,8 +25,14 @@ public class UserService implements UserDetailsService {
 	@Autowired
 	private UserRepository repository;
 	
+	@Autowired
+	private AuthService authService;
+	
 	@Transactional(readOnly = true)
 	public UserDTO findById(Long id) {
+		
+		authService.validateSelfOrAdmin(id);
+		
 		Optional<User> obj = repository.findById(id);
 		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new UserDTO(entity);
@@ -35,7 +41,7 @@ public class UserService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
+
 		User user = repository.findByEmail(username);
 		
 		if(user == null) {
